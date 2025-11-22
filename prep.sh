6ebb9262c905639f42cd6c2ba0b7de295e853070
Al Muhdil Karim <almuhdilkarim2@gmail.com>
2025-11-22 23:38:04 +0700

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -22,11 +22,11 @@ mkfs.vfat -F32 -S 4096 -n BOOT $DISKBOOT
 mkfs.ext4 -F -q -b 4096 /dev/mapper/proc &&
 mkfs.ext4 -F -q -b 4096 /dev/mapper/data &&
 
-mount /dev/proc /mnt &&
+mount /dev/mapper/proc /mnt &&
 mkdir -p /mnt/boot &&
 mount -o uid=0,gid=0,dmask=007,fmask=007 $DISKBOOT /mnt/boot/ &&
 mkdir -p /mnt/home &&
-mount /dev/data /mnt/home &&
+mount /dev/mapper/data /mnt/home &&
 
 pacstrap /mnt base &&
 
