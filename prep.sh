07eb9ada65a58ffed374d9a1e6d860c81d050b4e
Al Muhdil Karim <almuhdilkarim2@gmail.com>
2025-11-23 23:42:43 +0700

    update

--- prep.sh
+++ prep.sh
@@ -5,34 +5,34 @@ source /install/post/packer
 
 echo $DISKPROC;
 echo $DISKDATA;
-echo $DISTRO_INSTALLATION_PACKAGE;
-# if [[ ! -z $(findmnt --mountpoint /mnt) ]]; then 
-#  	umount -R /mnt
-# fi
 
-# if [[ ! -e /dev/mapper/proc ]]; then 
-# 	cryptsetup luksOpen $DISKPROC proc
-# fi
+if [[ ! -z $(findmnt --mountpoint /mnt) ]]; then 
+ 	umount -R /mnt
+fi
 
-# if [[ ! -e /dev/mapper/data  ]]; then 
-# 	cryptsetup luksOpen $DISKDATA data
-# fi
+if [[ ! -e /dev/mapper/proc ]]; then 
+	cryptsetup luksOpen $DISKPROC proc
+fi
 
-
-# mkfs.vfat -F32 -S 4096 -n BOOT $DISKBOOT
-# mkfs.ext4 -F -b 4096 /dev/mapper/proc &&
-# mkfs.ext4 -F -b 4096 /dev/mapper/data &&
-
-# mount /dev/mapper/proc /mnt &&
-# mkdir -p /mnt/boot &&
-# mount -o uid=0,gid=0,dmask=007,fmask=007 $DISKBOOT /mnt/boot/ &&
-# mkdir -p /mnt/home &&
-# mount /dev/mapper/data /mnt/home &&
-
-# pacstrap /mnt $DISTRO_INSTALLATION_PACKAGE &&
-
-# genfstab -U /mnt > /mnt/etc/fstab &&
-# cp -fr /install/post /mnt &&
+if [[ ! -e /dev/mapper/data  ]]; then 
+	cryptsetup luksOpen $DISKDATA data
+fi
 
 
-# arch-chroot /mnt /bin/bash /post/init.sh
+mkfs.vfat -F32 -S 4096 -n BOOT $DISKBOOT
+mkfs.ext4 -F -b 4096 /dev/mapper/proc &&
+mkfs.ext4 -F -b 4096 /dev/mapper/data &&
+
+mount /dev/mapper/proc /mnt &&
+mkdir -p /mnt/boot &&
+mount -o uid=0,gid=0,dmask=007,fmask=007 $DISKBOOT /mnt/boot/ &&
+mkdir -p /mnt/home &&
+mount /dev/mapper/data /mnt/home &&
+
+pacstrap /mnt $DISTRO_INSTALLATION_PACKAGE &&
+
+genfstab -U /mnt > /mnt/etc/fstab &&
+cp -fr /install/post /mnt &&
+
+
+arch-chroot /mnt /bin/bash /post/init.sh
